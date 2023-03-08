1e933b14c863887374f2292433aca8afc9efe6c0
magh <ghma1213@gmail.com>
2023-03-08 10:29:25 +0900

    Initial commit

---
+++ src/main/java/com/example/springbootdockergithubaction/SpringbootDockerGithubActionApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootdockergithubaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDockerGithubActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDockerGithubActionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootdockergithubaction/SpringbootDockerGithubActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootdockergithubaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDockerGithubActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
